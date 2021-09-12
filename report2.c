94a6cb8a0460089b3350ea8f64e1de087f74bd75
UniM0cha <solst_ice@naver.com>
2021-09-12 10:23:13 +0900

    report2

---
+++ report2.c
@@ -0,0 +1,23 @@
+#include <netdb.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+
+int main(int argc, char **argv)
+{
+  struct hostent *he;
+  struct in_addr addr;
+  
+  char *host = argv[1];
+
+  if (isalpha(host[0])){  // 주소가 도메인이면
+    he = gethostbyname(host);
+    printf("%s\n", inet_ntoa(addr));
+  }
+  else {                  // 주소가 아이피이면
+    addr.s_addr = inet_addr(host);
+    he = gethostbyaddr((char *) &addr, 4, AF_INET);
+    printf("%s\n", he->h_name);
+  }
+  return 0;
+}
